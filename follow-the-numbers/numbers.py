6906e249c9445b9ef1fd5f2bd75a1b13e65654e4
Raven842 <otherworldbadges@gmail.com>
2024-02-10 12:18:23 +1000

    updated numbers.py

--- follow-the-numbers/numbers.py
+++ follow-the-numbers/numbers.py
@@ -10,3 +10,17 @@ HEIGHT = 400
 dots = []
 lines = []
 next_dot = 0
+
+# Code
+for dot in range(0, 10):
+    actor = Actor("dot")
+    actor.pos = randint(20, (WIDTH - 20)), randint(20, (HEIGHT - 20))
+    dots.append(actor)
+
+def draw():
+    screen.fill("black")
+    number = 1
+    for dot in dots:
+        screen.draw.text(str(number), (dot.pos[0], dot.pos[1] + 12))
+        dot.draw()
+        number += 1
