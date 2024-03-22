9c95b5b256169fcbba9f7ebbc40db820bc8daa58
Raven842 <lavenderraven@duck.com>
2024-03-22 21:59:57 +1000

    Finished simple implimentation for C version of ceasar

--- cryptography/ceasar.c
+++ cryptography/ceasar.c
@@ -1,28 +1,45 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
 
-char * cipher(int k, char * p);
+char * cipher(int k, char p[]);
 
 int main(void) {
     int key;
-    char *plain;
+    char plain[1024];
     const int l = strlen(plain);
     printf("Key: ");
-    scanf("%i", &key);
+    scanf(" %i", &key);
+    while (getchar() != '\n');
     printf("Plaintext: ");
-    scanf("%s", plain);
+    fgets(plain, 1024, stdin);
     printf("Ciphertext: %s", cipher(key, plain));
 }
 
-char * cipher(int k, char * p) {
-    int l = strlen(p);
-    char * cipher;
+char * cipher(int k, char p[]) {
+    const int l = strlen(p);
+    char *cipher_text = malloc((l + 1) * sizeof(char));
     for (int i = 0; i < l; i++) {
-       cipher[i] = tolower(p[i]) + k;
-        if (cipher[i] > 122) {
-            cipher[i] -= 26;
-        }
+       if (isalpha(p[i]) == 0){
+        cipher_text[i] = p[i];
+       }
+       else {
+            if (islower(p[i]) > 0) {
+                cipher_text[i] = tolower(p[i]) + k;
+                if (cipher_text[i] > 122) {
+                    cipher_text[i] -= 26;
+                }
+            }
+            else {
+                cipher_text[i] = toupper(p[i]) + k;
+                if (cipher_text[i] > 90) {
+                    cipher_text[i] -= 26;
+                }                
+            }
+       }
     }
-    return cipher;
+    cipher_text[l] = '\0';
+    return cipher_text;
+    free(cipher_text);
 }
